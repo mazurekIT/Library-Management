a9572e95efd2a4d02360ee96967437e117dfc758
Mazzi <mazzi198913@wp.pl>
2019-07-31 22:54:15 +0200

    commit

---
+++ src/main/java/pl/mazurekit/cognifide/Book.java
@@ -0,0 +1,21 @@
+package pl.mazurekit.cognifide;
+
+import java.util.List;
+
+public class Book {
+    private String isbn;
+    private String title;
+    private String subtitle;
+    private String publisher;
+    private Long publishedDate;
+    private String description;
+    private Integer pageCount;
+    private String thumbnailUrl;
+    private String language;
+    private String previewLink;
+    private Double averageRating;
+    private List<String> authors;
+    private List<String> categories;
+
+
+}
